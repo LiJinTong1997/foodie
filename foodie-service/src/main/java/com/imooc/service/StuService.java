ac06a21bf7e402388da77b9d8595cc67aa420f8e
TinyDeer <919114921@qq.com>
2019-11-25 19:53:37 +0800

    Mybatis逆向工程
    Mybatis逆向工程创建Mapper以及各种实体类。
    同时测试了CURD功能以及事务的相关知识

---
+++ foodie-service/src/main/java/com/imooc/service/StuService.java
@@ -0,0 +1,22 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Stu;
+
+public interface StuService {
+
+    public Stu getStuInfo(int id);
+
+    public void saveStu();
+
+    public void updateStu(int id);
+
+    public void deleteStu(int id);
+
+    public void saveParent();
+
+    public void saveChildren();
+
+    public void saveChildren1();
+
+    public void saveChildren2();
+}
